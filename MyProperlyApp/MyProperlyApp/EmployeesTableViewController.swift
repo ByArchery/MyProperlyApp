7ee88ad7e605f3579c531cc84697b2388e820dad
David Andres Mejia Lopez <david.mejia@co-it005146.globant.com>
2019-09-09 11:48:37 -0500

    some changes

--- MyProperlyApp/MyProperlyApp/EmployeesTableViewController.swift
+++ MyProperlyApp/MyProperlyApp/EmployeesTableViewController.swift
@@ -10,11 +10,7 @@ import UIKit
 
 class EmployeesTableViewController: UITableViewController {
 
-    var employees: DataSourceFactory
-    
-    init(employees: DataSourceFactory) {
-        self.employees = employees
-    }
+    var employeesDataFromDataSource = DataSourceFactory()
     
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
@@ -22,7 +18,7 @@ class EmployeesTableViewController: UITableViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.employees = {
+        self.employeesDataFromDataSource = {
             DataSourceFactory.init()
         }()
     }
@@ -32,13 +28,13 @@ class EmployeesTableViewController: UITableViewController {
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return self.employees.obtainDataSource()
+        return DataSourceData.employeesAttributes.count
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "EmployeesCell", for: indexPath) as! EmployeeTableViewCell
         
-        cell.employee = self.employees.employees[indexPath.row]
+        cell.employee = employeesDataFromDataSource
 
         return cell
     }
